7db5a2711700826e25379dec78aed4eacbbaa160
Tu Nguyen <tuinfor@ymail.com>
2022-04-05 18:20:36 -0400

    display the current day on font page

--- js/script.js
+++ js/script.js
@@ -6,14 +6,3 @@ function displayCurrentDay() {
     todayDisplayEl.text(`Today ${today}`);
 }
 setInterval(displayCurrentDay, 1000);
-
-
-// First selector the class or id, html
-$("selector").metho("")
-
-
-// on lick
-$("b#utton").on("click", function(){
-    $("#button").fadeOut(200); //fadeout with 200ms 
-    //can change to  mouseover, fadeToggle(200) fadeIn toggle
-})
