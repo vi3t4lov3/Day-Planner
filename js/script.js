c425c1a4a182dd777581abbf21d498da66fb85f3
Tu Nguyen <tuinfor@ymail.com>
2022-04-05 23:13:01 -0400

    add the AM PM for time, & add the statement to change the color when the current time change

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
-var todayDisplayEl = $('#currentDay');
+    var todayDisplayEl = $('#currentDay');
+    var mainEL = $('container');
 
-//display the current day
+    //display the current day
 function displayCurrentDay() {
-    var today = moment().format('dddd DD, YYYY hh:mm:ss');
+    var today = moment().format('dddd DD, YYYY LT');
     todayDisplayEl.text(`Today ${today}`);
 }
 setInterval(displayCurrentDay, 1000);
 
-$('document').ready(function() {
-    var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+$('mainEL').ready(function() {
+    var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
     for (i = 0 ; i < businessHours.length; i++) {
         displayScheduleTimes(i, businessHours[i]);
     }
-
-// create a work schedule hours
+    // create a work schedule hours
 function displayScheduleTimes(i, businessHours) {
+    // display AM & PM
     var displayHours = businessHours > 12 ? businessHours - 12 : businessHours;
+    var dayNight = '';
+    if (businessHours < 12) {
+        dayNight = ' AM'
+    }
+    else {
+        dayNight =' PM';
+    }
     //display list of business hours 9 am to 5pm
-    var businessHoursColumn = $(`<div class='col-sm-2 timeCol'></div>`).text(displayHours + ':00');
+    var businessHoursColumn = $(`<div class='col-sm-2 timeCol'></div>`).text(displayHours + ':00' + dayNight).css({color: 'blue', fontWeight: 'bold'});''
     // display the scheduleNoteColumn
-    var scheduleNoteColumn = $(`<div class='col-sm-8 schedule' id='${businessHours + 'a'}'><input class='inputText'></div>`);
+    var scheduleNoteColumn = $(`<div class='col-sm-8' id='${businessHours + 'a'}'><input class='inputText'></div>`);
     //display the save button
     var saveColumn = $(`<div class='col-sm-2' id='save'><button type='button' class='btn btn-info' id='${businessHours + 'b'}'><i class='fas fa-save'></i></button></div>`);
     //display the row of 3 column above
@@ -29,17 +37,30 @@ function displayScheduleTimes(i, businessHours) {
     //appending row to the home page HTML
     $('.container').append(displayRow)
 
+    //getting the localStorage
     var savedNote = window.localStorage.getItem(businessHours + 'a');
     if (savedNote !== null) {
         $('#' + businessHours + 'a input').val(savedNote);
     }
 }
-$('.btn').on('click', function(){
-    var buttonId = $(this).attr('id');
-    var textId = buttonId.replace('b', 'a');
-    var textContent = $('#' + textId).children()[0].value;
-    // console.log(textContent)
-    localStorage.setItem(textId, textContent);
-});
+    //setting the key value to local storage
+    $('.btn').on('click', function(){
+        var buttonId = $(this).attr('id');
+        var textId = buttonId.replace('b', 'a');
+        var textContent = $('#' + textId).children()[0].value;
+        localStorage.setItem(textId, textContent);
+    });
+
+    // set color
+    var currentHour = moment().format('LT');
+    if (businessHours === currentHour) {
+        $('#' + businessHours).css('backgroundColor', 'red');
+    }
+    else if (businessHours > currentHour) {
+        // $('#' + businessHours).css('backgroundColor', 'blue');
+    }
+    else {
+        $('#' + businessHours).css('backgroundColor', 'yellow');
+    }
 
 });
